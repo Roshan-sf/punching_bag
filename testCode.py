fe40ea8c3bae76298cf57e882555ea52b055c9d8
g-drywall <gkoren22@gmail.com>
2021-02-22 13:17:31 -0800

    uptdated still problems with detecting button

--- testCode.py
+++ testCode.py
@@ -2,51 +2,59 @@ from gpiozero import LED, Button
 import time
 import random
 
-button = Button(14)
+button0 = Button(14)
+button1 = Button(15)
+LED0 = LED(12)
+LED1 = LED(16)
 red = LED(21)
 green = LED(20)
-indicator = LED(16)
-timeOn = 50
-timeOnCurrent = 0
-StillGoing = True
-won = False
-pressed = False
-
+answer = [2,2]
+guess = [2,2]
+ongoing = True
+def fillslot():
+    value = 2
+    while value == 2:
+        print(value)
+        if button0.is_pressed:
+            LED0.on()
+            value = 0
+            button0.wait_for_release()
+            LED0.off()
+        elif button1.is_pressed:
+            LED1.on()
+            value = 1
+            button1.wait_for_release()
+            LED1.off()
+    return value
+    
+while ongoing == True:
+    green.on()
+    time.sleep(2)
+    green.off()
+    print("green off")
+    for y in range(len(answer)):
+        x = answer[y]
+        x = random.randint(0,1)
+        answer[y] = x
+        if x == 0:
+            LED0.on()
+            time.sleep(1)
+            LED0.off()
+        elif x == 1:
+            LED1.on()
+            time.sleep(1)
+            LED1.off()
+    print(answer)
+    for x in guess:
+        x = fillslot()
+    if answer != guess:
+        ongoing == False
+    
+    
+red.on()
+sleep(2)
+#ending code
+LED0.off()
+LED1.off()
 red.off()
 green.off()
-indicator.off()
-
-while StillGoing == True:
-    indicator.on()
-    while timeOnCurrent < timeOn and pressed == False:
-        if button.is_pressed:
-            print("pressed")
-            pressed = True
-        else:
-            time.sleep(.1)
-            timeOnCurrent+=1
-    indicator.off()
-    print("indication done")
-    
-    if timeOnCurrent == timeOn and timeOn==0:
-        won = True
-        StillGoing = False
-    elif timeOnCurrent == timeOn:
-        red.on()
-        time.sleep(5)
-        StillGoing = False
-    elif pressed == True:
-        green.on()
-        time.sleep(2)
-        green.off()
-        time.sleep(1)
-        pressed = False
-
-    timeOn = timeOn-1
-    timeOnCurrent = 0
-    
-    wait = random.randint(5,70)
-    print(wait)
-    wait = wait*.1
-if won == True:
-    green.blink()
